c7a539a6ce4c4cf513855668a2289dd4eb9ee255
JustinKwok13 <jkwok51@my.bcit.ca>
2020-10-17 15:55:31 -0700

    MVP Architecture
    Put the location logic into the Interactor and created interactor, presenter, and view classes.

--- app/src/main/java/com/example/a4guysphotogallery/MainActivity.java
+++ app/src/main/java/com/example/a4guysphotogallery/MainActivity.java
@@ -49,7 +49,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainActivityView {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     String mostRecentPhoto;
 
@@ -60,8 +60,8 @@ public class MainActivity extends AppCompatActivity {
     List<String> photoPaths;
     int curIndex;
 
-    private FusedLocationProviderClient fusedLocationClient;
-    LocationCallback locationCallback;
+    private MainActivityPresenter presenter;
+    private MainActivityInteractor interactor;
 
     @SuppressLint("MissingPermission")
     @Override
@@ -73,26 +73,9 @@ public class MainActivity extends AppCompatActivity {
         captionText = findViewById(R.id.captionText);
         captionBtn = findViewById(R.id.captionBtn);
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        fusedLocationClient.getLocationAvailability()
-                .addOnSuccessListener(this, new OnSuccessListener<LocationAvailability>() {
-                    @Override
-                    public void onSuccess(LocationAvailability locationAvailability) {
-                        Log.d("locAvail", "Location is available");
-                    }
-                });
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    return;
-                }
-                for (Location location : locationResult.getLocations()) {
-                    // Update UI with location data
-                    // ...
-                }
-            }
-        };
+        interactor = new MainActivityInteractor(this, mostRecentPhoto);
+        presenter = new MainActivityPresenter(interactor);
+        presenter.bind(this);
 
         Bundle bundle = getIntent().getExtras();
         Long startDate = null;
@@ -121,42 +104,6 @@ public class MainActivity extends AppCompatActivity {
         }
         showPhoto(mostRecentPhoto);
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        startLocationUpdates();
-    }
-
-    LocationRequest locationRequest = new LocationRequest();
-
-    private void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Log.d("permissions disabled","");
-            return;
-        }
-        fusedLocationClient.requestLocationUpdates(locationRequest,
-                locationCallback,
-                Looper.getMainLooper());
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        stopLocationUpdates();
-    }
-
-    private void stopLocationUpdates() {
-        fusedLocationClient.removeLocationUpdates(locationCallback);
-    }
-
 
     public void sendSearch(View view) {
         Intent intent = new Intent(this, SearchActivity.class);
@@ -252,60 +199,7 @@ public class MainActivity extends AppCompatActivity {
 
             Log.d("exifpathoar" , mostRecentPhoto);
 
-            fusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            // Got last known location. In some rare situations this can be null.
-                            if (location != null) {
-                                Log.d("lastloc", location.getLatitude() + ", " + location.getLongitude());
-
-                                ExifInterface exif;
-                                try {
-                                    exif = new ExifInterface(mostRecentPhoto);
-                                    Log.d("exif status", exif.TAG_FILE_SOURCE);
-                                    exif.setGpsInfo(location);
-                                    //exif.setAttribute(ExifInterface.TAG_GPS_LATITUDE, Double.toString(location.getLatitude()));
-
-                                    //exif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE, Double.toString(location.getLongitude()));
-
-                                    try {
-                                        exif.saveAttributes();
-                                        Log.d("exifsaved","exif saved");
-                                        String lat = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
-
-                                        String lng = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
-
-                                        Log.d("exiftest", "lat: " + lat + ", lng: " + lng);
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                        Log.d("exifsavefail","exif save failed");
-                                        String lat = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
-
-                                        String lng = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
-
-                                        Log.d("exiftestfail", "lat: " + lat + ", lng: " + lng);
-                                    }
-
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                    Log.d("ioexception", "except io");
-                                } catch (NullPointerException e) {
-                                    e.printStackTrace();
-                                    Log.d("nullexception", "except null");
-                                }
-
-                            } else {
-                                Log.d("lastlocfail", "locnull");
-                            }
-                        }
-                    }).addOnFailureListener(this, new OnFailureListener() {
-                        public void onFailure(Exception e) {
-                            Log.d("fusedfail", e.toString());
-                        }
-                    });
-                    ;
-
+            interactor.picture(this);
             photoPaths = getPhotos();
         }
     }
